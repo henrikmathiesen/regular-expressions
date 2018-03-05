cd86f38d13e2b96af347a857a3a6a4f622158906
henrikmathiesen <henrik_mathiesen@hotmail.com>
2018-03-05 19:15:24 +0100

    -updated doks -some simple regex

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # regular-expressions
 Its time to learn them ...
+
+## Doks
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
--- jesting.test.js
+++ jesting.test.js
@@ -1,5 +1,19 @@
 describe('Jest should work', () => {
-    it('should do a unit test', () => { 
+    it('should do a unit test', () => {
         expect(true).toBe(true);
+    });
+
+    it('should do one regex tha returns true', () => { 
+        const pattern = /abc/;
+        const subject = 'The latest airplane designs evolved from slabcraft';
+
+        expect(pattern.test(subject)).toBe(true);
+    });
+
+    it('should do one regex tha returns false', () => { 
+        const pattern = /abc/;
+        const subject = 'Grab crab';
+
+        expect(pattern.test(subject)).toBe(false);
     });
 });
