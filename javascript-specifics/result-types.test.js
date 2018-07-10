394a776b277b70f58d0d02369254f186ecf56456
henrikmathiesen <henrik_mathiesen@hotmail.com>
2018-07-10 08:01:29 +0200

    -cleaned up code -str.search

--- javascript-specifics/result-types.test.js
+++ javascript-specifics/result-types.test.js
@@ -1,35 +1,43 @@
-describe('Result types', () => {
+describe('API and Result types', () => {
 
     const subject = 'The latest airplane designs evolved from slabcraft';
     const pattern = /abc/;
 
-    it('should understand how to get a boolean, indicating Does it match', () => {
-        expect(pattern.test(subject)).toBe(true);
-    });
+    describe('query from a regex pattern, sending in a string', () => {
+        it('should understand how to get a boolean, indicating Does it match', () => {
+            expect(pattern.test(subject)).toBe(true);
+        });
 
-    it('should understand how to get an array, indicating How many matches ', () => {
-        const result = pattern.exec(subject);
+        it('should understand how to get an array, indicating How many matches ', () => {
+            const result = pattern.exec(subject);
 
-        expect(result.length).toBe(1);
-        expect(result[0]).toBe('abc');
-    });
+            expect(result.length).toBe(1);
+            expect(result[0]).toBe('abc');
+        });
 
-    it('should understand how to get an array, indicating How many matches, this way', () => {
-        const result = subject.match(pattern);
-        expect(result.length).toBe(1);
-        expect(result[0]).toBe('abc');
-    });
-
-    describe('no matches with array return type', () => {
-        it('should understand that the return type will be null of no match -- A', () => { 
+        it('should understand that the return type will be null of no match', () => {
             const result = /xyz/.exec(subject);
             expect(result).toBe(null);
         });
+    });
 
-        it('should understand that the return type will be null of no match -- B', () => { 
+    describe('query from a string, sending in regex', () => {
+        it('should understand that search returns the index of the position of the match', () => { 
+            const result = subject.search(pattern);
+            expect(result > 0).toBe(true);
+        });
+
+        it('should understand how to get an array, indicating How many matches', () => {
+            const result = subject.match(pattern);
+            expect(result.length).toBe(1);
+            expect(result[0]).toBe('abc');
+        });
+
+        it('should understand that the return type will be null of no match', () => {
             const result = subject.match(/xyz/);
             expect(result).toBe(null);
         });
     });
+
 
 });
