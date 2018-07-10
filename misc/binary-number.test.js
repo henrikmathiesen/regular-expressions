ff327a25077ac4a2fcdd2175980b8c22ff9c441d
henrikmathiesen <henrik_mathiesen@hotmail.com>
2018-07-10 07:50:41 +0200

    -removed character class -comments about result

--- misc/binary-number.test.js
+++ misc/binary-number.test.js
@@ -1,15 +1,20 @@
 describe('input must be binary', () => {
 
-    const pattern = /^([0]|[1])+$/;
+    const pattern = /^(0|1)+$/;
 
     it('should test 1', () => { 
         const result = '10010011'.match(pattern);
+        // actually get 2 matches in the array
+        // - one item matching the whole string
+        // - one item matching last digit
+        // If adding g flag the matches array will only contain the first item
+        // Iam not sure ...
         expect(result.length).toBeTruthy();
     });
 
     it('should test 2', () => { 
         const result = '10010012'.match(pattern);
-        expect(result).toBe(null);
+        expect(result).toBe(null); 
     });
 
     it('should test 3', () => { 
