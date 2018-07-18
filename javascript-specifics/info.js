87548fa59324dd3eec86b0b68cbbf55c229a3ad9
henrikmathiesen <henrik_mathiesen@hotmail.com>
2018-07-18 12:18:14 +0200

    -restructuring

--- javascript-specifics/info.js
+++ javascript-specifics/info.js
@@ -1,8 +1,23 @@
 /*
 
+
+
+    MODIFIERS
+
     /g enables "global" matching. When using the replace() method, specify this modifier to replace all matches, rather than only the first one.
     /i makes the regex match case insensitive.
     /m enables "multi-line mode". In this mode, the caret and dollar match before and after newlines in the subject string.
 
     https://www.regular-expressions.info/javascript.html
+
+    
+    
+    QUANTIFIERS
+
+    ? match 0-1 times
+    * match 0-more times
+    + match 1-more times
+
+
+
 */
---
+++ simple-repetition/basics-02.test.js
@@ -0,0 +1,12 @@
+describe('Basics 02', () => {
+
+    it('should understand capturing groups 01', () => { 
+        /*
+            Lets say we want to search for all name.png files, but we only want to return the name
+            We can use a capturing group to capture just the name
+        */
+
+        
+    });
+
+});
